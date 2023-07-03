9124888babcb9a0f8bfb1046c316bf7670ccc246
Pablo Dezuani <pablodezuani@gmail.com>
2023-07-03 17:00:38 -0300

    g4g4g4g4g4g

--- src/shared/components/text/text.style.ts
+++ src/shared/components/text/text.style.ts
@@ -4,12 +4,13 @@ interface ContainerTextProps{
 
 color? : string;
 fontsize: string;
+fontFamily: 'Poppins-Bold' |'Poppins-Light' |'Poppins-Regular';
 
 }
 export const ContainerText  = styled.Text<ContainerTextProps>`
 ${(props) => props.color? `color: ${props.color}`:""}
 
-font-family:Raleway-Bold;
+font-family:${(props) => props.fontFamily  };
 color:#8C2A96;
 font-size:${(props)=> props.fontsize};
 
--- src/shared/components/text/text.tsx
+++ src/shared/components/text/text.tsx
@@ -1,5 +1,5 @@
 
-import {Text, TextProps as TextPropsNative} from"react-native/types";
+import {Keyboard, Text, TextProps as TextPropsNative} from"react-native/types";
 import { ContainerText } from "./text.style";
 import { textTypes } from "./textTypes";
 import { useMemo } from "react";
@@ -9,15 +9,54 @@ interface TextProps extends TextPropsNative{
   
 }
 const text = ({color,type,...props}:TextProps) =>{
-const handlefontsize = useMemo(() =>{
+const fontsize = useMemo(() =>{
     switch (type) {
-        case textTypes.TITLE:
-            return '16px';
-            default:
-            return'16px';
+        case textTypes.TITLE_BOLD:
+        case textTypes.TITLE_LIGHT:
+        case textTypes.TITLE_REGULAR:
+            return '24px';
+        case textTypes.SUB_TITLE_BOLD:
+        case textTypes.SUB_TITLE_LIGHT:
+        case textTypes.SUB_TITLE_REGULAR:
+            return '20px';
+    
+                
+        case textTypes. PARAGRAPH_SMALL_BOLD:
+        case textTypes. PARAGRAPH_SMALL_LIGHT:
+        case textTypes.PARAGRAPH_SMALL_REGULAR:
+                return '10px';
+        case textTypes. PARAGRAPH_BOLD:
+        case textTypes. PARAGRAPH_LIGHT:
+        case textTypes. PARAGRAPH_REGULAR:
+         default:
+            return '14px';
+
     }
 },[type]);
-    
-return <ContainerText fontsize ={handlefontsize}{...props}/>;
+
+const fontFamily = useMemo( () => {
+    switch (type) {
+    case textTypes.TITLE_BOLD:
+    case textTypes.SUB_TITLE_BOLD:
+    case textTypes. PARAGRAPH_SMALL_BOLD:
+    case textTypes. PARAGRAPH_BOLD:
+return 'Poppins-Bold';
+case textTypes.TITLE_LIGHT:
+    case textTypes.SUB_TITLE_LIGHT:
+        case textTypes. PARAGRAPH_SMALL_LIGHT:
+            case textTypes. PARAGRAPH_LIGHT:
+                return 'Poppins-Light'
+                case textTypes.TITLE_REGULAR:
+    case textTypes.SUB_TITLE_REGULAR:
+    case textTypes.PARAGRAPH_SMALL_REGULAR:
+    case textTypes. PARAGRAPH_REGULAR:
+
+default:
+return 'Poppins-Regular';
+
+    }
+},[type]);
+
+return <ContainerText fontFamily={fontFamily }fontsize ={fontsize}{...props}/>;
 };
 export default text;
