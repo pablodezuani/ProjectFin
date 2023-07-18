de7afe0e0fd472630f9748e9b13bdf36643fc0d9
Pablo Dezuani <pablodezuani@gmail.com>
2023-07-18 12:44:13 -0300

    redux começo

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { SafeAreaView, View } from 'react-native';
 import Login from './src/modules/login';
+import store from '././src/store'
+import { Provider } from 'react-redux'
 
 const App = () => {
   return (
+    <Provider store={store}>
     <SafeAreaView> 
   <Login />
 </SafeAreaView>
-      
-  )
+</Provider>
+  );
   
 };
 export default App;
