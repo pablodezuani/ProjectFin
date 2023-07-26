0e9dfb4858f7c382f9e97d2ffb618b614ac6e15b
Pablo Dezuani <pablodezuani@gmail.com>
2023-07-26 11:29:38 -0300

    criação

---
+++ src/shared/components/modal/modal.tsx
@@ -0,0 +1,35 @@
+import { Modal as ModalReact, ModalProps as ModalPropsReact } from 'react-native';
+
+import { theme} from '../themes/theme'
+import Button from '../button/Button';
+import Text from '../text/Text';
+import { textTypes } from '../text/textTypes';
+import { modalTestId } from './__mocks__/modal.tesid'
+import { ContainerModal, IconCloseModal } from './modal.style';
+
+interface ModalProps extends ModalPropsReact {
+  title: string;
+  text: string;
+  onCloseModal: () => void;
+}
+
+const Modal = ({ title, text, onCloseModal, ...props }: ModalProps) => {
+  return (
+    <ModalReact animationType="slide" transparent={true} onRequestClose={onCloseModal} {...props}>
+      <ContainerModal>
+        <Text
+          testID={modalTestId.MODAL_TITLE}
+          type={textTypes.PARAGRAPH_SEMI_BOLD}
+          color={theme.colors.graytheme.gray100}
+        >
+          {title}
+        </Text>
+        <Text testID={modalTestId.MODAL_TEXT}>{text}</Text>
+        <Button testID={modalTestId.MODAL_CLOSE_BUTTON} title="OK" onPress={onCloseModal} />
+        <IconCloseModal testID={modalTestId.MODAL_CLOSE_ICON} onPress={onCloseModal} name="cross" />
+      </ContainerModal>
+    </ModalReact>
+  );
+};
+
+export default Modal;
