d1d1c97a0c7b16d94dfb1595621b875ce9c75877
Pablo Dezuani <pdezuani@universal.org>
2023-08-30 12:49:17 -0300

    add async

---
+++ src/shared/components/functions/connection/storageProxy.ts
@@ -0,0 +1,13 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Stringifier } from 'styled-components/native/dist/types';
+export const setItemStorage = async (key: string, value:string)=>
+AsyncStorage.setItem(key, value);
+
+export const getItemStorage = async (key:string)=>AsyncStorage.getItem(key).catch(()=> '');
+
+export const removeItemStorage = async (key:string)=>AsyncStorage.removeItem(key).catch();
+
+
+
+
+ 
